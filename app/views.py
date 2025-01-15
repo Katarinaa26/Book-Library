4084e9ba519888cf33f11d7cad788552104dae30
Katarinaa26 <kaca.grujic00@gmail.com>
2025-01-15 17:50:12 +0100

    Add sign-up and log in functionality

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, request,flash, redirect, url_for
+from flask_login import login_required, current_user
+from .models import User
+from . import db
+
+views = Blueprint("views", __name__)
+
+@views.route("/")
+@views.route("/home")
+@login_required
+def home():
+    return render_template("home.html", user=current_user)
